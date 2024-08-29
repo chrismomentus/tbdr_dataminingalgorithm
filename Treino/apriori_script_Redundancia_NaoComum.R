304fb27b011d1cda5063c174f2ef35e5039f204b
chrismomentus <chrismomentus@gmail.com>
2024-08-29 09:24:43 -0300

    rodada de script de treino

--- Treino/apriori_script_Redundancia_NaoComum.R
+++ Treino/apriori_script_Redundancia_NaoComum.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(stringr)
 library("ggplot2")
 library(tibble)
+library(tidyr)
 
 ##############################################
 #data praparations
@@ -32,9 +33,8 @@ df_sample <- function(dt_frame1,seed_val){
 ###################
 
 Apriori_func <- function(dt_frame, seed_apriori){
-  
-  set.seed(seed_apriori)
   trans_treino <- as(dt_frame, "transactions")
+  set.seed(seed_apriori)
   reg_treino <- apriori(data = trans_treino, parameter = list(supp = 0.03, conf = 0.05), 
   appearance <- list(rhs=c("encerra2=0","encerra2=1", "encerra2=2","encerra2=3"), default = "lhs"), control = list(verbose=F))
   
@@ -64,6 +64,37 @@ rule_prep <- function(rule_dt){
   return(data_rules)
 }
 #######################################
+freq_RegrasNaoComum <- function(df){
+  require(plyr)
+  qual<- c("LHSandRHS")
+  countedCombos <- plyr::ddply(df, qual, nrow )
+  
+  names(countedCombos) <- c(qual, "Frequencia")
+  
+  countedCombos <- countedCombos[with(countedCombos, order(-Frequencia)),]
+  write.csv(countedCombos, "Apriori_Redundancia_NaoComum/NaoComum/RegrasNaoComum.csv")
+  return(countedCombos)
+}
+
+freq_RegrasComum <- function(df){
+  require(plyr)
+  qual<- c("LHSandRHS")
+  countedCombos <- plyr::ddply(df, qual, nrow )
+  
+  names(countedCombos) <- c(qual, "Frequencia")
+  
+  countedCombos <- countedCombos[with(countedCombos, order(-Frequencia)),]
+  write.csv(countedCombos, "Apriori_Redundancia_NaoComum/NaoComum/RegrasComum.csv")
+  return(TRUE)
+}
+BuscaBinario <- function(df1, df2){
+  df1 <- df1[order(df1$LHSandRHS,decreasing = TRUE),]
+  df1 <- df1 %>% distinct(LHSandRHS, .keep_all = TRUE)
+  
+  resultingDF <- data.frame(cbind(df1, Frequencia = df2$Frequencia))
+  
+  return(resultingDF)
+}
 regrasNaoCoincedente <- function(df1, df2, nomearq){
   grau = paste("NaoCoincedentegrau", nomearq,"arq.csv", sep = "-")
   
@@ -71,9 +102,9 @@ regrasNaoCoincedente <- function(df1, df2, nomearq){
     anti_join(df1, df2, by='LHSandRHS'),
     anti_join(df2, df1, by='LHSandRHS'),
   )
-  Nrow_val<- nrow(Nao_coincedente)
+  #Nrow_val<- nrow(Nao_coincedente)
   write.csv(Nao_coincedente, paste("Apriori_Redundancia_NaoComum/naoCoincedente", grau, sep = "/"))
-  return(Nrow_val)
+  return(Nao_coincedente)
 }
 
 ###############################################
@@ -86,9 +117,9 @@ function_valida <- function(df,seed_sample, seed_apriori, nomearq){
   
   regrasApriori <- Apriori_func(df_bootstrap, seed_apriori)
  
-  regrasElimRedun <- regrasApriori[ !is.redundant( regrasApriori, measure="confidence" ) ]
+  #regrasElimRedun <- regrasApriori[ !is.redundant( regrasApriori, measure="confidence" ) ]
   
-  regrasPreparadas<- rule_prep(regrasElimRedun)
+  regrasPreparadas<- rule_prep(regrasApriori)
 
   #Pegar varios em cima de lift 1.1
   regrasPreparadas <- subset(regrasPreparadas, lift >= 1.1, select= c(LHSandRHS,count,lift, confidence, support))
@@ -96,15 +127,24 @@ function_valida <- function(df,seed_sample, seed_apriori, nomearq){
   return(regrasPreparadas)
 }
 
-run = 60
+##------------Separete Desfechos to Column-------------
+splitDesfechos2ColumnFunction <- function(DF){
+  uncomumDFSplit <- data.frame(do.call('rbind', strsplit(as.character(DF$LHSandRHS),' ',fixed=TRUE)))
+  rmColLHSandRHS <- select(DF,-c("LHSandRHS"))
+  splitDF <- data.frame(cbind(setNames(uncomumDFSplit["X1"], "LHSandRHS"), setNames(uncomumDFSplit["X3"], "Desfechos"), rmColLHSandRHS)) 
+  return(splitDF)
+}
+
+###############################################
+run = 100
 valor <- data.frame( matrix(nrow = run, ncol = 2))
 
 for (i in 1:1) {
   
-  seedValue1 = as.numeric(format(Sys.Date(), format="%d"))
-  seedValue2 = as.numeric(format(Sys.Date(), format="%d"))*2
+  seedValue1 = as.numeric(format(Sys.Date(), format="%m"))
+  seedValue2 = seedValue1*2
   
-  coincedente = function_valida(df, seedValue1, seedValue2, i)
+  coincedente = function_valida(df, seedValue1, seedValue2)
   
   GeneralInfo <- data.frame(runCount=i, ValorSeed_1=seedValue1, ValorSeed_2=seedValue2, Coincedente1=nrow(coincedente), Coincedente2=0, RegraComum=nrow(coincedente), RegraNaoComum=0)
   
@@ -112,20 +152,29 @@ for (i in 1:1) {
   valor[i,2] <- nrow(coincedente)
   print(paste(valor[i,1:2]))
   
+  rbind_RegrasNaoComum = data.frame()
+  #rbind_RegrasComum = data.frame()
+  
   for (k in 2:run) {
     
     seedValue1 = seedValue1+3
     seedValue2 = seedValue2+3
     Regras_valida1 <- coincedente
-    Regras_valida2 <- function_valida(df, seedValue1, seedValue2, k)
+    Regras_valida2 <- function_valida(df, seedValue1, seedValue2)
     
     #Intersect Das k frames de Regras 
-    coincedente = merge(coincedente, Regras_valida2, by.x="LHSandRHS", by.y = "LHSandRHS")
+    
+    coincedente = merge(Regras_valida2, coincedente, by.x="LHSandRHS", by.y = "LHSandRHS")
     coincedente_df = as_tibble(coincedente, .name_repair = "minimal")
     
-    nrow_naocomum <- regrasNaoCoincedente(Regras_valida2,coincedente_df,k)
+    df_naocomum <- regrasNaoCoincedente(Regras_valida2,coincedente_df,k)
     
-    GeneralInfo[nrow(GeneralInfo)+1,] <- list(k, seedValue1, seedValue2, nrow(Regras_valida1), nrow(Regras_valida2) ,nrow(coincedente), nrow_naocomum)
+    GeneralInfo[nrow(GeneralInfo)+1,] <- list(k, seedValue1, seedValue2, nrow(Regras_valida1), nrow(Regras_valida2) ,nrow(coincedente), nrow(df_naocomum))
+    
+    rbind_RegrasNaoComum <- rbind(rbind_RegrasNaoComum, df_naocomum[,1:5])
+    
+    #rbind_RegrasComum <- rbind(rbind_RegrasComum, coincedente_df[,1:5])
+    
     
     valor[k,1] <- k
     valor[k,2] <- nrow(coincedente)
@@ -135,86 +184,70 @@ for (i in 1:1) {
       return(print("Criterio de parada encontrado"))
     
   }
-  plot(valor,type = "o")
+  #plot(valor,type = "o")
   
 }
+######################
+
 write.csv(GeneralInfo, "Apriori_Redundancia_NaoComum/coincedente/GeneralInformacaoTreino.csv")
 #NOTE:: Exporta o arquivo e separe o atributo "Desfecho" para outra coluna
+#coincedenteDesfechos <- splitDesfechos2ColumnFunction(coincedente)
 write.csv(coincedente, "Apriori_Redundancia_NaoComum/coincedente/coincedente.csv")
 #-----------------------
 #-------------------------
 
 coincedentes <- read.csv(file="Apriori_Redundancia_NaoComum/coincedente/coincedente.csv", header = TRUE, sep =",")
 
-#newfile1 <- within(coincedente0.1, LHSandRHS<-data.frame(do.call('rbind', strsplit(as.character(LHSandRHS), ' ', fixed=TRUE))))
-newfile1 <- data.frame(do.call('rbind', strsplit(as.character(coincedentes$LHSandRHS),' ',fixed=TRUE)))
-newfile1$X2 = NULL
-newfile2 <- select(coincedentes,-c(1,2))
-newfile3 <- coincedentes$X
-newfile <-  data.frame(cbind(newfile3, newfile1, newfile2)) 
-names(newfile)[1] <- "ID"
-names(newfile)[2] <- "LHSandRHS"
-names(newfile)[3] <- "Desfechos"
+##-------------Coincedente Nao comum <99%--------
+FrequenciaRegrasNaoComum <- freq_RegrasNaoComum(rbind_RegrasNaoComum)
+FrequenciaRegrasNaoComumDesfechos <- splitDesfechos2ColumnFunction(FrequenciaRegrasNaoComum)
 
-write.csv(newfile, "Apriori_Redundancia_NaoComum/coincedente/newcoincedente0.03&0.05.csv")
+NaoComumDistinct <- BuscaBinario(rbind_RegrasNaoComum, FrequenciaRegrasNaoComum)
+write.csv(NaoComumDistinct, "Apriori_Redundancia_NaoComum/EliminaRedundancia/NaoCoincedente0.03&0.05.csv")
 
 
-desfechos0 <- subset(newfile, Desfechos == "encerra2=0", select=(ID:support.y.1))
-desfechos1 <- subset(newfile, Desfechos == "encerra2=1", select=(ID:support.y.1))
-desfechos2 <- subset(newfile, Desfechos == "encerra2=2", select=(ID:support.y.1))
-desfechos3 <- subset(newfile, Desfechos == "encerra2=3", select=(ID:support.y.1))
+################################################################
+################################################################
+#--------Função que Elimina Redeundancia de NAO-COMUM----
 
-write.csv(desfechos0, "Apriori_Redundancia_NaoComum/arquivo/desf0FullRules.csv")
-write.csv(desfechos1, "Apriori_Redundancia_NaoComum/arquivo/desf1FullRules.csv")
-write.csv(desfechos2, "Apriori_Redundancia_NaoComum/arquivo/desf2FullRules.csv")
-write.csv(desfechos3, "Apriori_Redundancia_NaoComum/arquivo/desf3FullRules.csv")
-
-#----------------------------------------------------------------
-
-df <- select(newfile,ID, LHSandRHS)
-
-df4 <- df %>% mutate(ID =  row_number(), flg = 1) %>%
-  separate_rows(LHSandRHS, sep = ",") %>%
-  spread(LHSandRHS, flg)
-
-df4$a=NULL
-a <- select(newfile, Desfechos:support.y)
-matrix_coin <-  data.frame(cbind(df4, a)) 
-
-desfechos00 <- subset(matrix_coin, Desfechos == "encerra2=0", select=(ID:support.y))
-desfechos11 <- subset(matrix_coin, Desfechos == "encerra2=1", select=(ID:support.y))
-desfechos22 <- subset(matrix_coin, Desfechos == "encerra2=2", select=(ID:support.y))
-desfechos33 <- subset(matrix_coin, Desfechos == "encerra2=3", select=(ID:support.y))
-
-
-write.csv(desfechos00, "Apriori_Redundancia_NaoComum/desfecho/desf0Cura_coincedenteMatrix.csv")
-write.csv(desfechos11, "Apriori_Redundancia_NaoComum/desfecho/desf1Abandono_coincedenteMatrix.csv")
-write.csv(desfechos22, "Apriori_Redundancia_NaoComum/desfecho/desf2Falencia_coincedenteMatrix.csv")
-write.csv(desfechos33, "Apriori_Redundancia_NaoComum/desfecho/desf3Obito_coincedenteMatrix.csv")
-
-
-###################TODO
-#Count each item and save into new df
-#Plot a graph showing
-desfechoCura <- desfechos00 %>% select( -c(Desfechos:ncol(desfechos00)))
-desfechoAbandon <- desfechos11 %>% select( -c(Desfechos:ncol(desfechos11)))
-desfechoFalencia <- desfechos22 %>% select( -c(Desfechos:ncol(desfechos22)))
-desfechoObito <- desfechos33 %>% select( -c(Desfechos:ncol(desfechos33)))
-
-matrizToFreqGraph <- function(matrixDF){
-  countAttr <- sapply(matrixDF, FUN=table)
-  countedItemsDF <- as.data.frame(t(as.data.frame(do.call(cbind, countAttr))))
-  countedItemsDF <- tibble::rownames_to_column(countedItemsDF, "items")
-  colnames(countedItemsDF)[2] = "frequencies"
-  decreOrdfreqDataSet <- countedItemsDF[order(countedItemsDF$frequencies, decreasing=TRUE),]
-  return(barplot(decreOrdfreqDataSet$frequencies[1:10], names.arg = decreOrdfreqDataSet$items[1:10]))
+leftDF <- NaoComumDistinct
+count2 <- 0
+leftDF$confidence <- round(leftDF$confidence, 2)
+i <- 1
+while(i <= nrow(leftDF)) {
+  selfDF_split <- unlist(strsplit(leftDF[i,"LHSandRHS"], "[, ]+"))
+  selfLength <- length(selfDF_split)
+  
+  for (l in 1:nrow(leftDF)) {
+    count2 <- l
+    downDF_Split <- unlist(strsplit(leftDF[l,"LHSandRHS"], "[, ]+"))
+    downLength <- length(downDF_Split)
+    
+    for (k in 1:selfLength) {
+      
+      if(selfDF_split[k] %in% downDF_Split){
+        
+        if(k == selfLength && !is.na(leftDF[i,"confidence"])){
+          selfDF_conf <- leftDF[i,"confidence"]
+          downDF_conf <- leftDF[l,"confidence"]
+          
+          if(selfDF_conf >= downDF_conf && selfLength < downLength){
+            leftDF <- leftDF[-l,]
+            
+          } 
+          
+        } 
+      } else {
+        break
+      }
+    }
+  }
+  
+  print(paste("Loop ", i, " of loop ", l," and row of", nrow(leftDF) ))
+  i <- i+1
 }
 
-matrizToFreqGraph(desfechoCura)
-matrizToFreqGraph(desfechoAbandon)
-matrizToFreqGraph(desfechoFalencia)
-matrizToFreqGraph(desfechoObito)
+#write.csv(leftDF, "Apriori_TesteTreino_NaoComum/EliminaRedundancia/ElimRedundanciaNaoComum.csv")
 
-
-
-
+RedundanciaNaoComumSplit <- splitDesfechos2ColumnFunction(leftDF)
+write.csv(RedundanciaNaoComumSplit, "Apriori_Redundancia_NaoComum/EliminaRedundancia/NaoCoincedenteElimRedundancia.csv")
